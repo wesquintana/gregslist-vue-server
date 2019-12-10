0440b4116cf831ed35c1b81fa3645cdfcde714d9
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-12-10 12:25:07 -0700

    Completed Cars

---
+++ server/models/Car.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const Car = new Schema(
+  {
+    make: { type: String, required: true, maxlength: 20 },
+    model: { type: String, required: true },
+    price: { type: Number, required: true },
+    year: { type: Number, max: 2040, min: 1800 },
+    imgUrls: [{ type: String }],
+    description: { type: String, maxlength: 250 }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+export default Car;
---
+++ server/services/CarService.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import Car from "../models/Car";
+import ApiError from "../utils/ApiError";
+
+const _repository = mongoose.model("Car", Car);
+
+class CarService {
+  async getAll() {
+    return await _repository.find({});
+  }
+  async getById(id) {
+    let data = await _repository.findById(id);
+    if (!data) {
+      throw new ApiError("Invalid ID", 400);
+    }
+    return data;
+  }
+
+  async create(rawData) {
+    let data = await _repository.create(rawData);
+    return data;
+  }
+
+  async edit(id, update) {
+    let data = await _repository.findOneAndUpdate({ _id: id }, update, {
+      new: true
+    });
+    if (!data) {
+      throw new ApiError("Invalid ID", 400);
+    }
+    return data;
+  }
+
+  async delete(id) {
+    let data = await _repository.findOneAndDelete({ _id: id });
+    if (!data) {
+      throw new ApiError("Invalid ID", 400);
+    }
+  }
+}
+
+const carService = new CarService();
+export default carService;
